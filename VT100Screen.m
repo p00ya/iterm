08b2e6e318376dc981d27175cdb2b31e9adb51a6
Fabian Y <yfabian@users.sourceforge.net>
2003-01-07 21:50:44 +0000

    *** empty log message ***

--- VT100Screen.m
+++ VT100Screen.m
@@ -922,7 +922,7 @@ static BOOL PLAYBELL = YES;
     case 0:
         i=idx=[self getIndex:CURSOR_X y:CURSOR_Y];
         s=[STORAGE string];
-        for(i=0;i<[STORAGE length]&&[s characterAtIndex:i]!='\n';i++);
+        for(;i<[s length]&&[s characterAtIndex:i]!='\n';i++);
         if (i>idx) [STORAGE deleteCharactersInRange:NSMakeRange(idx,i-idx)];
         
         //[self setStringSpaceToX:CURSOR_X Y:CURSOR_Y length:WIDTH-CURSOR_X];
