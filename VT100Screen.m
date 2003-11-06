db0235bf1ea023866cd634e04d96c0a0ea2f8a32
Ujwal S. Setlur <ujwal@users.sourceforge.net>
2003-11-06 16:05:33 +0000

    * Fixed bug causing incorrect number of columns.

--- VT100Screen.m
+++ VT100Screen.m
@@ -145,7 +145,7 @@ static BOOL PLAYBELL = YES;
 #endif
     sz = [VT100Screen fontSize:font];
 #if USE_CUSTOM_LAYOUT
-    w = (int)(frame.size.width  - 1*[VT100Typesetter lineFragmentPadding])/sz.width;
+    w = (int)(frame.size.width  - 2*[VT100Typesetter lineFragmentPadding])/sz.width;
 #else
     w = (int)(frame.size.width / sz.width + 0.5) - 2;
 #endif
