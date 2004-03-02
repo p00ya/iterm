efc8f090c4c07b3959b1bd2b2079e1969feea531
Ujwal S. Setlur <ujwal@users.sourceforge.net>
2004-03-02 08:22:47 +0000

    * Added more code for keybinding manager. Still incomplete, but getting there.

--- PreferencePanel.m
+++ PreferencePanel.m
@@ -427,7 +427,7 @@ static float versionNumber;
 
 - (void)_addKBProfileSheetDidEnd:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo
 {
-	if(returnCode == NSOKButton)
+	if(returnCode == NSOKButton && [[kbProfileName stringValue] length] > 0)
 	{
 		NSEnumerator *kbProfileEnumerator;
 		NSString *aString;
